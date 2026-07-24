844f4de2db578d235ab07659b6a2951075fd8bb6
ramizalam <ramizalam63@gmail.com>
2026-07-24 17:37:58 +0530

    frontend completed

--- client/src/pages/AIComposer.tsx
+++ client/src/pages/AIComposer.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
-import { dummyGenerationData } from "../assets/assets";
-import { ArrowRightIcon, HistoryIcon, Loader2Icon, SparkleIcon, XIcon } from "lucide-react";
+import { dummyGenerationData, PLATFORMS } from "../assets/assets";
+import {
+  ArrowRightIcon,
+  CalendarIcon,
+  ClockIcon,
+  HistoryIcon,
+  Loader2Icon,
+  SparkleIcon,
+  TimerIcon,
+  XIcon,
+} from "lucide-react";
 
 const AIComposer = () => {
   const [prompt, setPrompt] = React.useState<string>("");
@@ -10,7 +19,7 @@ const AIComposer = () => {
   const [generations, setGenerations] = React.useState<any[]>([]);
 
   //scheduling state
-  const [activeScheduler, setActiveScheduler] = React.useState(null);
+  const [activeScheduler, setActiveScheduler] = React.useState<any>(null);
   const [selectedPlatform, setSelectedPlatform] = React.useState<string[]>([]);
   const [scehduleDate, setScheduleDate] = React.useState<string>("");
   const [scehduleTime, setScheduleTime] = React.useState<string>("");
@@ -30,6 +39,13 @@ const AIComposer = () => {
       setLoading(false);
       setGenerations(dummyGenerationData);
     }, 2000);
+  };
+
+  const handleSchedule = async ()=>{
+    setScheduling(true);
+    setTimeout(()=>{
+      setScheduling(false)
+    },2000)
   }
 
   const tones = [
@@ -44,7 +60,6 @@ const AIComposer = () => {
     "Creative",
     "Optimistic",
   ];
-  
 
   return (
     <div className="max-w-4xl mx-auto p-4 space-y-12 pb-20 animate-in fade-in duration-700">
@@ -76,7 +91,11 @@ const AIComposer = () => {
               </div>
             </button>
 
-                <button disabled={loading} className="bg-slate-900 hover:bg-slate-900 text-white flex items-center gap-2 px-4 py-2 rounded-lg transition-colors" onClick={handleGenerate}>
+            <button
+              disabled={loading}
+              className="bg-slate-900 hover:bg-slate-900 text-white flex items-center gap-2 px-4 py-2 rounded-lg transition-colors"
+              onClick={handleGenerate}
+            >
               {loading ? (
                 <>
                   <Loader2Icon className="size-4  animate-spin" />
@@ -93,7 +112,11 @@ const AIComposer = () => {
         </div>
         <div className="flex items-center gap-3 justify-center flex-wrap">
           {tones.map((toneOption) => (
-            <button key={toneOption} onClick={() => setTone(toneOption)} className={`px-4 py-2 rounded-full text-sm transition-all ${tone === toneOption ? "bg-red-500 border-red-500 text-white" : "bg-slate-100 text-slate-800 hover:bg-slate-200"}`}>
+            <button
+              key={toneOption}
+              onClick={() => setTone(toneOption)}
+              className={`px-4 py-2 rounded-full text-sm transition-all ${tone === toneOption ? "bg-red-500 border-red-500 text-white" : "bg-slate-100 text-slate-800 hover:bg-slate-200"}`}
+            >
               {toneOption}
             </button>
           ))}
@@ -102,71 +125,155 @@ const AIComposer = () => {
 
       {/* AI Generated Post */}
       <div className="space-y-6 pt-12 border-t border-slate-200">
-        <div className="flex items-center justify-between text-slate-800" >
-           <div className="flex items-center gap-3 mb-2">
+        <div className="flex items-center justify-between text-slate-800">
+          <div className="flex items-center gap-3 mb-2">
             <HistoryIcon className="size-5" />
             <h2 className="text-xl">Recent Generations</h2>
-           </div>
-           <span className=" text-sm text-slate-500 bg-slate-50" >{generations.length} total</span>
+          </div>
+          <span className=" text-sm text-slate-500 bg-slate-50">
+            {generations.length} total
+          </span>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 xl:grid-cols-3 gap-6">
           {generations.map((generation, index) => (
-            <div key={index} className="group bg-white rounded 2-xl border border-slate-100 p-5 hover:border-red-200 transition-all relative overflow-hidden">
+            <div
+              key={index}
+              className="group bg-white rounded 2-xl border border-slate-100 p-5 hover:border-red-200 transition-all relative overflow-hidden"
+            >
               <div className="flex flex-col h-full space-y-3">
-                 <div className="flex  items-center justify-between">
-                   <span className="text-xs text-slate-400 uppercase tracking-widest">{new Date(generation.createdAt).toLocaleString()}</span>
-                   <span className="text-xs text-red-500 bg-red-50 px-2 py-0.5 rounded.md">{generation.tone}</span>
-                 </div>
-                 <p className="text-sm text-slate-600 line-clamp-3 leading-relaxed flex-1">{generation.content}</p>
+                <div className="flex  items-center justify-between">
+                  <span className="text-xs text-slate-400 uppercase tracking-widest">
+                    {new Date(generation.createdAt).toLocaleString()}
+                  </span>
+                  <span className="text-xs text-red-500 bg-red-50 px-2 py-0.5 rounded.md">
+                    {generation.tone}
+                  </span>
+                </div>
+                <p className="text-sm text-slate-600 line-clamp-3 leading-relaxed flex-1">
+                  {generation.content}
+                </p>
                 {generation.mediaUrl && (
                   <div>
-                    <img src={generation.mediaUrl} alt="gen"  className="w-full aspect-video object-cover opacity-90 group-hover:opacity-100 transition opacity"/>
+                    <img
+                      src={generation.mediaUrl}
+                      alt="gen"
+                      className="w-full aspect-video object-cover opacity-90 group-hover:opacity-100 transition opacity"
+                    />
                   </div>
                 )}
                 <div className="flex items-center gap-2 pt-2">
                   <button
-                  onClick={()=> setActiveScheduler(generation)}
-                   className="flex-1 bg-slate-100 hover:bg-red-500 hover:text-white text-slate-600 text-xs py-2.5 rounded-lg transition-all">
+                    onClick={() => setActiveScheduler(generation)}
+                    className="flex-1 bg-slate-100 hover:bg-red-500 hover:text-white text-slate-600 text-xs py-2.5 rounded-lg transition-all"
+                  >
                     Scheduled Post
                   </button>
                 </div>
               </div>
             </div>
           ))}
-          {
-            generations.length===0 &&( 
-              <div className="col-span-full py-20 text-center space-y-2">
-                <div className="size-12 bg-slate-50 rounded-2xl flex items-center justify-center mx-auto text-slate-300">
-                  <SparkleIcon className="size-6"/>
-                </div>
-                <p className="col-span-full py-20 text-center space-y-2">No content generated at. try generating some content using AI.</p>
+          {generations.length === 0 && (
+            <div className="col-span-full py-20 text-center space-y-2">
+              <div className="size-12 bg-slate-50 rounded-2xl flex items-center justify-center mx-auto text-slate-300">
+                <SparkleIcon className="size-6" />
               </div>
-            )
-          }
+              <p className="col-span-full py-20 text-center space-y-2">
+                No content generated at. try generating some content using AI.
+              </p>
+            </div>
+          )}
         </div>
       </div>
- 
+
       {/* Scheduler Modal */}
       {activeScheduler && (
         <div className="fixed inset-0 min-h-screen z-50 flex items-center justify-center p-4 bg-slate-900/40 backdrop-blur-md animate-in fade-in duration-300">
           <div className="bg-white rounded-2xl shadow-2xl w-full max-w-2xl border border-slate-100 overflow-hidden flex flex-col max-h-[90vh] ">
-
             <div className="flex items-center justify-between px-8 py-4 border-b border-slate-100 bg-slate-50/30">
-               <h3 className="text-slate-900">Schedule Generation</h3>
-               <button onClick={()=>setActiveScheduler(null)} className="p-2 rounded-full hover:bg-slate-100 text-slate-400 transition-colors">
-                <XIcon className="size-5"/>
-                </button>
+              <h3 className="text-slate-900">Schedule Generation</h3>
+              <button
+                onClick={() => setActiveScheduler(null)}
+                className="p-2 rounded-full hover:bg-slate-100 text-slate-400 transition-colors"
+              >
+                <XIcon className="size-5" />
+              </button>
             </div>
             <div className="flex-1 overflow-y-auto p-8  space-y-4 ">
-            <div className="bg-slate-50 rounded-2xl p-6 border border-slate-100 space-y-4">
-              <p>{activeScheduler.prompt}</p>
+              <div className="bg-slate-50 rounded-2xl p-6 border border-slate-100 space-y-4">
+                <p>{activeScheduler.prompt}</p>
+              </div>
+
+              <div className="bg-slate-50 rounded-2xl p-6 border border-slate-100 space-y-4">
+                <p>{activeScheduler.content}</p>
+                {activeScheduler.mediaUrl && (
+                  <img
+                    src={activeScheduler.mediaUrl}
+                    alt="preview"
+                    className="w-full aspect-video object-cover rounded-xl border border-slate-100 shadow-sm"
+                  />
+                )}
+              </div>
             </div>
 
-            <div className="bg-slate-50 rounded-2xl p-6 border border-slate-100 space-y-4">
-              <p>{activeScheduler.content}</p>
-              {activeScheduler.mediaUrl && <img src={activeScheduler.mediaUrl} alt="preview" className="w-full aspect-video object-cover rounded-xl border border-slate-100 shadow-sm"/>}
+            <div className="p-8 bg-slate-50/50 border-t border-slate-50 space-y-8">
+              {/* options */}
+              <div className="space-y-6">
+                <div>
+                  <label
+                    className="block text-xs text-slate-600 uppercase tracking-widest mb-4"
+                    htmlFor=""
+                  >
+                    Select Channels
+                  </label>
+                  <div className="flex flex-wrap gap-2">
+                    {PLATFORMS.map((p) => {
+                      const active = selectedPlatform.includes(p.id);
+                      return (
+                        <button
+                          key={p.id}
+                          onClick={() =>
+                            setSelectedPlatform((prev) =>
+                              prev.includes(p.id)
+                                ? prev.filter((x) => x !== p.id)
+                                : [...prev, p.id],
+                            )
+                          }
+                          className={`p-2.5 rounded-md border text-xs ${active ? "bg-red-500/80 text-white" : "bg-white border-slate-200 text-slate-200 text-slate-400 hover:border-slate-300"} `}
+                        >
+                          <p.icon className="size-4.5" />
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+                  <div className="relative">
+                    <CalendarIcon className="size-4 absolute left-4 top-1/2 -translate-y-1/2 text-slate-400" />
+                    <input
+                      type="date"
+                      className="w-full pl-11 pr-4 py-3 bg-slate-50 border border-slate-100 rounded-md text-slate-900 text-sm foucus:outline-none transition-all"
+                      value={scehduleDate}
+                      onChange={(e) => setScheduleDate(e.target.value)}
+                    />
+                  </div>
+                    <div className="relative">
+                    <ClockIcon className="size-4 absolute left-4 top-1/2 -translate-y-1/2 text-slate-400" />
+                    <input
+                      type="time"
+                      className="w-full pl-11 pr-4 py-3 bg-slate-50 border border-slate-100 rounded-md text-slate-900 text-sm foucus:outline-none transition-all"
+                      value={scehduleTime}
+                      onChange={(e) => setScheduleTime(e.target.value)}
+                    />
+                  </div>
+                </div> 
+              </div>
+              <button  onClick={handleSchedule} className="w-full flex items-center justify-center gap-2 py-3 rounded-md bg-slate-200 text-slate-700 hover:bg-red-500 hover:text-white transition">{scheduling?<Loader2Icon className="size-4 animate spin"/>:<TimerIcon className="size-4"/>}
+              Schedule Post
+              </button>
             </div>
-            </div>
+
+
           </div>
         </div>
       )}
