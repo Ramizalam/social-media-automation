9468a51804bbe54c0349f4da690548e557dae2f1
ramizalam <ramizalam63@gmail.com>
2026-08-01 14:54:59 +0530

    update readme

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -4,7 +4,7 @@ const postSchema = new mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId , ref:"Account",required:true},
     content: {type: String, required: true},
     mediaUrl : {type: String},
-    mediaType: {type: String ,enum : ["image","video"]},
+    mediaType: {type: String ,enum : ["image","video","none"]},
     platform : [{type: String, enum : ["instagram","twitter","facebook" ,"X"]}],
     scheduledFor : {type: Date,required : true},
     status : {type:String, enum : ["draft","scheduled","published","failed"],default:"scheduled"},   
